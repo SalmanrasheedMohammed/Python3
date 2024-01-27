87115d89fe2fd88a31bdc862db9e4e94f427e204
salmanrasheedmd <msalmanrasheed2000@gmail.com>
2024-01-27 10:34:44 +0530

    v1

---
+++ DataTypes/D2.py
@@ -0,0 +1,58 @@
+# LIST
+
+a = [ 0, 1, 2, 3, 4, 5]
+b = [ 'a', 'b', 'c', 'd']
+
+print (a)
+print (b)
+print (type(a))
+
+#OUTPUT 
+# [0, 1, 2, 3, 4, 5]
+# ['a', 'b', 'c', 'd']
+# <class 'list'>
+
+# Number System
+# Binary
+
+binary = 0b0101010
+print (binary)
+
+#output 
+# 42
+
+# hexa
+hexadecimal = 0xace
+print (hexadecimal)
+
+# output 
+# 2766
+
+# octal
+octal = 0o123
+print (octal)
+
+# output 
+# 83
+
+# build in functions
+# binary
+b1 =bin(10)
+print (b1)
+
+# output 
+# 0b1010
+
+# hexa
+h1 =hex(10)
+print (h1)
+
+# output
+# 0xa
+
+#octal
+o1 = oct(10)
+print (o1)
+
+# output
+# 0o12cdcd
