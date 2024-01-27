9b1d55e30028c0ff1d459d97524221439b3b8555
salmanrasheedmd <msalmanrasheed2000@gmail.com>
2024-01-27 10:32:33 +0530

    v1

---
+++ DataTypes/D1.py
@@ -0,0 +1,96 @@
+#INT
+
+a = 10
+b = 20
+
+print (a, b)
+print (a+b)
+print (a-b)
+print (a*b)
+print (type(a))
+
+#OUTPUT
+# 10 20
+# 30
+# -10
+# 200
+# <class 'int'>
+
+
+
+#FLOAT
+
+ab = 100.00
+bc = 200.00
+
+print (ab, bc)
+print (type(ab))
+
+#OUTPUT 
+# 100.0 200.0 
+# <class 'float'>
+
+#STR 
+
+s = "rasheed"
+j = "jannu"
+
+print (s, j)
+print (type(s))
+
+#OUTPUT 
+# rasheed jannu
+# <class 'str'>
+
+#BOOL
+
+ab = False
+bc = True
+
+print (ab)
+print (bc)
+print (type(ab))
+
+#OUTPUT 
+# False
+# True
+# <class 'bool'>
+
+#LIST
+
+a = [ 0, 1, 2, 3, 4, 5]
+b = [ 'a', 'b', 'c', 'd']
+
+print (a)
+print (b)
+print (type(a))
+
+#OUTPUT 
+# [0, 1, 2, 3, 4, 5]
+# ['a', 'b', 'c', 'd']
+# <class 'list'>
+
+#DICT
+
+person1 = {'name': 'rasheed','age': '23','gender': 'male','city': 'kakinada'}
+person2 = {'name': 'jannu','age': '23','gender': 'male','city': 'pitapurama'}
+
+print (person1)
+print (person2)
+print (type(person1))
+
+#OUTPUT 
+# {'name': 'rasheed', 'age': '23', 'gender': 'male', 'city': 'kakinada'}
+# {'name': 'jannu', 'age': '23', 'gender': 'male', 'city': 'pitapurama'}
+# <class 'dict'>
+
+#set
+
+a = { 0, 1, 2, 3, 4, 5}
+
+print (a)
+print (type(a))
+
+#OUTPUT 
+# {0, 1, 2, 3, 4, 5}
+# <class 'set'>
