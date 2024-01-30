f6d001dbfa801995d3f542360c7bc83c6eb1a942
salmanrasheedmd <msalmanrasheed2000@gmail.com>
2024-01-30 16:46:49 +0530

    v1

--- PythonClass/Datatypes/D3.py
+++ PythonClass/Datatypes/D3.py
@@ -1,7 +1,10 @@
 # index(element)
 
 list = [1,2,3,4,5]
-print(list.index(2)) 
-# 1
-print(list.index(5)) 
-# 4
+#index( 0 1 2 3 4)
+
+print(list.index(2)) # 1
+print(list.index(5)) # 4
+
+print(list[1]) #2
+print(list[2]) #3
