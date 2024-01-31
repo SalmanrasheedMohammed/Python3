a7ed886d7f6a6dfb4580e44120d7c0db7517dc6d
salmanrasheedmd <msalmanrasheed2000@gmail.com>
2024-01-31 10:13:12 +0530

    v1

--- PythonClass/List/L2.py
+++ PythonClass/List/L2.py
@@ -1,3 +1,15 @@
-# LIST (APPEND, EXTEND, INSERT)
+# LIST (APPEND, EXTEND)
 
-#APPEND
+list = [1,2,3]
+print(list) # [1, 2, 3]
+
+# APPEND ( It will add the data at the end of the list)
+
+list.append(4)
+print(list) # [1, 2, 3, 4]
+
+# Extend ( it will add the multiple data)
+
+list.extend([5,6,7,8])
+print(list) # [1, 2, 3, 4, 5, 6, 7, 8]
+
---
+++ PythonClass/List/L3.py
@@ -0,0 +1,6 @@
+# INSERT ( it will add the data at the given index)
+
+list = [1, 2, 3, 4, 5, 6, 7, 8]
+list.insert(4, 6)
+
+print(list) # [1, 2, 3, 4, 6, 5, 6, 7, 8]
